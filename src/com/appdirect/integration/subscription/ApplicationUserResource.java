0a3ba1ce3fb6f7e6922e1b62b98aca672450368d
Akshay Sarvankar <akshay.sarvankar@gmail.com>
2017-07-17 00:48:55 -0400

    HeartBeat url added at /appIntegration/integration/userEvent/heartBeat

--- src/com/appdirect/integration/subscription/ApplicationUserResource.java
+++ src/com/appdirect/integration/subscription/ApplicationUserResource.java
@@ -5,7 +5,6 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -31,6 +30,15 @@ public class ApplicationUserResource extends AppResource {
 
 	@Autowired
 	private UserEventManager userEventManager= null;
+	
+	@GET
+	@Path("heartBeat")
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public Response heartBeat(@Context UriInfo uri) throws Exception{
+		Response response = new Response(true);
+		return response;
+		}
+	
 
 	@POST
 	@Path("userAssignment")
